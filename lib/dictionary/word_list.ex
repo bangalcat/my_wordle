282e467ba709033f5397bc90a237f24aba40cfb7
donghyun <semaj9310@gmail.com>
2022-02-13 13:37:09 +0900

    feat: dictionary word_list

---
+++ lib/dictionary/word_list.ex
@@ -0,0 +1,13 @@
+defmodule Dictionary.WordList do
+  @spec random_word(list(String.t())) :: String.t()
+  def random_word(word_list) do
+    Enum.random(word_list)
+  end
+
+  def words_list do
+    "../../assets/dictionary/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(" ", trim: true)
+  end
+end
